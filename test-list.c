ddba28624bd54a5c9cc06c7cf89fa167ede4b755
Esdras Diniz <linux.esdras@gmail.com>
2016-07-09 16:58:09 -0300

    Update test

--- test-list.c
+++ test-list.c
@@ -3,9 +3,25 @@
 #include <string.h>
 #include "list.h"
 
-static void print(void *data)
+
+static void print_list(list_t *list) 
 {
-	printf("%s ", (char*)data);
+	node_t *node;
+
+	LIST_FOR_EACH_NEXT(node, list) {
+		printf("%s ", (char *)node->data);
+	}
+	printf("\n");
+}
+
+static void print_list_rev(list_t *list)
+{
+	node_t *node;
+
+	LIST_FOR_EACH_PREV(node, list) {
+		printf("%s ", (char *)node->data);
+	}
+	printf("\n");
 }
 
 static int compare(void *a, void *b)
@@ -17,7 +33,7 @@ int main(void)
 {
 
 	list_t *list = list_new();
-	list_t *copy_list = NULL;
+	//list_t *copy_list = NULL;
 
 	node_t * node = NULL;
 
@@ -26,25 +42,25 @@ int main(void)
 	list_add_head(list, "3");
 	list_add_head(list, "4");
 
-	copy_list = list_add_tail(list, "5");
+	list_add_tail(list, "5");
+	list_add_tail(list, "6");
+	list_add_tail(list, "7");
 
-	list_add_tail(copy_list, "6");
-	list_add_tail(copy_list, "7");
+	// copy_list = list_add_tail(list, "5");
+
+	// list_add_tail(copy_list, "6");
+	// list_add_tail(copy_list, "7");
 
 
-	list_iterator_next(list, print);
-	puts("");
-
-	list_iterator_prev(copy_list, print);
-	puts("");
-
+	print_list(list);
+	print_list_rev(list);
 
 	list_remove_head(list);
 	list_remove_tail(list);
+	// list_remove_head(list);
+	// list_remove_head(list);
 
-
-	list_iterator_next(list, print);
-	puts("");
+	//list_destroy(list);
 
 	
 	node = list_find(list, "5", compare);
@@ -53,11 +69,9 @@ int main(void)
 		printf("FIND: %s\n", (char*)node->data); 
 
 
-	list_remove_node(copy_list, node);
+	list_remove_node(list, node);
 
-	list_iterator_next(list, print);
-	puts("");
-
+	print_list(list);
 
 	printf("COUNT: %d\n", list->length);
 
